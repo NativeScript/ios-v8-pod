fe41598f50d00c20d0b55b8e7ffdd918b0a198d6
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-02-04 15:41:07 +0200

    add build phase

--- add_build_phases.rb
+++ add_build_phases.rb
@@ -1,1 +1,7 @@
-puts "BUILD PHASES SCRIPT EXECUTED FORM NATIVESCRIPT"
+require 'xcodeproj'
+path_to_project = "${SOURCE_ROOT}/${PROJECT_NAME}.xcodeproj"
+project = Xcodeproj::Project.open(path_to_project)
+main_target = project.targets.first
+phase = main_target.new_shell_script_build_phase("NativeScript Phase")
+phase.shell_script = "do sth with ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/your.file"
+project.save()
