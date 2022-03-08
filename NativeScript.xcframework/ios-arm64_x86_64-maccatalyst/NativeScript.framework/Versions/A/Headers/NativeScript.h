753e6f4f75dbaed61aaaac964145a0bc7a3e8c9f
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-03-08 14:44:21 +0200

    Add updated ruby script with capacitor changes

--- NativeScript.xcframework/ios-arm64_x86_64-maccatalyst/NativeScript.framework/Versions/A/Headers/NativeScript.h
+++ NativeScript.xcframework/ios-arm64_x86_64-maccatalyst/NativeScript.framework/Versions/A/Headers/NativeScript.h
@@ -1,20 +1,3 @@
-#import <Foundation/Foundation.h>
-
-@interface Config : NSObject
-
-@property (nonatomic, retain) NSString* BaseDir;
-@property (nonatomic, retain) NSString* ApplicationPath;
-@property (nonatomic) void* MetadataPtr;
-@property BOOL IsDebug;
-@property BOOL LogToSystemConsole;
-@property int ArgumentsCount;
-@property (nonatomic) char** Arguments;
-
-@end
-
-@interface NativeScript : NSObject
-
-- (instancetype)initWithConfig:(Config*)config;
-- (bool)liveSync;
-
-@end
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7fcd4807e25c684087ba30faf2117a283dd84afd9b09d966575fce1bcbf07bd
+size 456
