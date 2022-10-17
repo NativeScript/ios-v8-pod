7842d03f12e37b2be471f6c9c86d6d918634e2f2
tdermendjiev <tdermendjievft@gmail.com>
2022-10-17 15:54:05 +0300

    Update NativeScript version

--- NativeScript.xcframework/ios-arm64/NativeScript.framework/Headers/NativeScript.h
+++ NativeScript.xcframework/ios-arm64/NativeScript.framework/Headers/NativeScript.h
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02455b90d13c350bd6f846b0fa4ab8cd9a5596947629dd02cc0e593d973380c4
-size 549
+#import <Foundation/Foundation.h>
+
+@interface Config : NSObject
+
+@property (nonatomic, retain) NSString* BaseDir;
+@property (nonatomic, retain) NSString* ApplicationPath;
+@property (nonatomic) void* MetadataPtr;
+@property BOOL IsDebug;
+@property BOOL LogToSystemConsole;
+@property int ArgumentsCount;
+@property (nonatomic) char** Arguments;
+
+@end
+
+@interface NativeScript : NSObject
+
+- (instancetype)initWithConfig:(Config*)config;
+/**
+ WARNING: this method does not return in most applications. (UIApplicationMain)
+ */
+- (void)runMainApplication;
+- (bool)liveSync;
+
+@end
