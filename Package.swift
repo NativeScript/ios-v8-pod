ada9cf3c96d80ae8af83a660e897d4d9be911884
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-04-04 12:24:46 +0300

    Remove SDK target

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "NativeScriptSDK",
+    name: "NativeScript",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NativeScriptSDK",
-            targets: ["NativeScriptSDK", "NativeScript"]),
+            name: "NativeScript",
+            targets: ["NativeScript"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,9 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NativeScriptSDK",
-            dependencies: []),
+        // .target(
+        //     name: "NativeScriptSDK",
+        //     dependencies: []),
         .binaryTarget(
             name: "NativeScript",
             url: "https://github.com/NativeScript/ios-v8-pod/releases/download/spm-test/NativeScript.xcframework.zip",
